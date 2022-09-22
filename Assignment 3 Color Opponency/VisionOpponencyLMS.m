0745f1470ce1b6d65ac1a8985681d7da2500ae60
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-09-22 12:12:56 -0400

    Assignment 3 - Calculated the LMS of the light source

--- Assignment 3 Color Opponency/VisionOpponencyLMS.m
+++ Assignment 3 Color Opponency/VisionOpponencyLMS.m
@@ -10,6 +10,8 @@ lms_matrix = transpose(lms_dataset{:,[2:4]});
 normalize_s_factor = 100*ones(light_sources_numRows,1); %normalise the matrix values to 0-1
 incandescent_source = light_sources_dataset{:,2}./ normalize_s_factor;
 daylight_source= light_sources_dataset{:,3} ./ normalize_s_factor;
+change_in_wavelength = mean(diff(light_sources_dataset{:,1}))
+
 
 
 % create a diagonal matrix to calculate illuminant's spectral power distribution = S𝜆
@@ -21,31 +23,44 @@ s_daylight_matrix = diag(daylight_source);
 r_matrix = color_checker_dataset{:,[2:color_checker_numCols]};
 
 % LMS values for all 24 ColorChecker samples
-LMS_incandescent = transpose(lms_matrix * s_incandescent_matrix * r_matrix);
-LMS_daylight = transpose(lms_matrix * s_daylight_matrix * r_matrix);
+LMS_incandescent_stimuli = transpose(lms_matrix * s_incandescent_matrix * r_matrix);
+LMS_daylight_stimuli = transpose(lms_matrix * s_daylight_matrix * r_matrix);
 
-LMS_incandescent
-LMS_daylight
+
+LMS_incandescent_stimuli
+LMS_daylight_stimuli
 
 %-----------------------------------------
 % Chromatic Adaptation Transformations (CAT)
 
+% Equi-energy light source is L1
+% Incandescent is L2
+
+% LMS_daylight_lightsource
+LMS_daylight_lightsource = transpose(lms_matrix * s_daylight_matrix)
+LMS_incandescent_lightsource = transpose(lms_matrix * s_incandescent_matrix)
+
+
+
+
 % Getting signals of a stimulus that appears white
 white_col_number = find(string(color_checker_dataset.Properties.VariableNames) == "White");
-LMS_white_incandescent = transpose(LMS_incandescent(white_col_number,:));
-LMS_white_daylight = transpose(LMS_daylight(white_col_number,:));
+LMS_white_incandescent = transpose(LMS_incandescent_stimuli(white_col_number,:));
+LMS_white_daylight = transpose(LMS_daylight_stimuli(white_col_number,:));
 
 % D_factor (complete chromatic adaptation = 1)
 degree_of_adaptation = 1;
 
-m_von_kries_incadescent_factor = diag((degree_of_adaptation*LMS_white_incandescent)+...
-    ((1-degree_of_adaptation)*LMS_white_daylight))./(LMS_white_daylight);
 
-m_von_kries_daylight_factor = diag((degree_of_adaptation*LMS_white_daylight)+...
-    ((1-degree_of_adaptation)*LMS_white_incandescent))./(LMS_white_incandescent);
 
-LMS_daylight_von_kries = transpose(m_von_kries_daylight_factor * transpose(LMS_incandescent));
-LMS_incandescent_von_kries = transpose(m_von_kries_incadescent_factor * transpose(LMS_daylight));
+% m_von_kries_incadescent_factor = diag((degree_of_adaptation*LMS_white_incandescent)+...
+%     ((1-degree_of_adaptation)*LMS_white_daylight))./(LMS_white_daylight);
+% 
+% m_von_kries_daylight_factor = diag((degree_of_adaptation*LMS_white_daylight)+...
+%     ((1-degree_of_adaptation)*LMS_white_incandescent))./(LMS_white_incandescent);
+
+LMS_daylight_von_kries = transpose(m_von_kries_daylight_factor * transpose(LMS_incandescent_stimuli));
+LMS_incandescent_von_kries = transpose(m_von_kries_incadescent_factor * transpose(LMS_daylight_stimuli));
 
 LMS_incandescent_von_kries
 LMS_daylight_von_kries
