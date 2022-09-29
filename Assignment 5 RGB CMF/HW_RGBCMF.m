5cdf29bbccab5c64b9386365840e821e812551d9
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-09-28 21:01:53 -0400

    Assignment 5 - Working on Question 4 & 5

--- Assignment 5 RGB CMF/HW_RGBCMF.m
+++ Assignment 5 RGB CMF/HW_RGBCMF.m
@@ -1,5 +1,6 @@
 lms_dataset = readtable("HW_Opponency_Data.xlsx",Sheet="LMS");
 rgb_spd_dataset = readtable("HW_DisplaySPD_Data.xlsx",Sheet="DisplaySPD");
+cie_dataset = readtable("HW_DisplaySPD_Data.xlsx",Sheet="CIE 1931");
 
 % Create LMS relative sensitivity matrix
 lms_matrix = transpose(lms_dataset{:,[2:4]});
@@ -19,11 +20,13 @@ LMS_RGB = LMS_RGB/M
 
 
 %color matching functions of the display’s primaries
-CMF_RGB = transpose(inv(LMS_RGB)*lms_matrix);
+CMF_RGB = inv(LMS_RGB)*lms_matrix;
 
 %Normalizing CMF by peak value
-C = max(CMF_RGB, [], 'all')
-CMF_RGB = CMF_RGB/C
+C = max(CMF_RGB, [], 'all');
+CMF_RGB = (CMF_RGB/C)
+CMF_RGB_transpose = transpose(CMF_RGB)
+
 
 
 colors=['r','g','b'];
@@ -31,7 +34,7 @@ labels = ['Red','Green','Blue']
 
 for i = 1:3
     hold on
-    plot(lms_dataset{:,1},CMF_RGB(:,i),"LineWidth",2,"Color",colors(i))
+    plot(lms_dataset{:,1},CMF_RGB_transpose(:,i),"LineWidth",2,"Color",colors(i))
 end
 grid on
 hold off
@@ -40,3 +43,42 @@ ylabel('Tristimulus Values')
 xlim([380 730])
 title('Color Matching Functions of Primaries')
 
+
+
+%Converting from CMFs to CIE standard colorimeter observer
+
+
+
+% Fixing residual error in calculating
+CIE_source = cie_dataset{:,[2:4]};
+
+% x_lamda = transpose(CIE_source(:,1))*pinv(CMF_RGB)
+y_lamda = transpose(CIE_source(:,2))*pinv(CMF_RGB)
+% z_lamda = transpose(CIE_source(:,3))*pinv(CMF_RGB)
+% m_function = [x_lamda;y_lamda;z_lamda]
+
+
+
+
+n_denominator = y_lamda(1)*(CMF_RGB_transpose(:,1)) + y_lamda(2)*(CMF_RGB_transpose(:,2)) + y_lamda(3)*(CMF_RGB_transpose(:,3))
+
+CIE_source(:,2)
+n_denominator
+% n_lamda = cie_xyz(:,2)*transpose(n_denominator)
+% 
+% 
+% transpose(m_function*(CMF_RGB*n_lamda))
+
+% cie_dataset(:,"y_bar")
+% CMF_RGB(:,1)
+
+
+%Least Squares(
+% transpose(cie_xyz(:,2))*pinv(CMF_RGB)
+
+
+
+
+
+
+% y_bar = cie_dataset{:,[2]}
