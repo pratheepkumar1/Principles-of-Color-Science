9a8a4b59660b30251f34a7ea416cff9322a38f9a
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-09-30 21:38:20 -0400

    Assignment 1 & 3 - Fixing Changes

--- Assignment 3 Color Opponency/VisionOpponencyLMS.m
+++ Assignment 3 Color Opponency/VisionOpponencyLMS.m
@@ -10,7 +10,7 @@ lms_matrix = transpose(lms_dataset{:,[2:4]});
 normalize_s_factor = 100*ones(light_sources_numRows,1); %normalise the matrix values to 0-1
 incandescent_source = light_sources_dataset{:,2}./ normalize_s_factor;
 daylight_source= light_sources_dataset{:,3} ./ normalize_s_factor;
-change_in_wavelength = mean(diff(light_sources_dataset{:,1}))
+n_lamda = mean(diff(light_sources_dataset{:,1}))
 
 
 
@@ -23,8 +23,8 @@ s_daylight_matrix = diag(daylight_source);
 r_matrix = color_checker_dataset{:,[2:color_checker_numCols]};
 
 % LMS values for all 24 ColorChecker samples
-LMS_incandescent_stimuli = transpose(lms_matrix * s_incandescent_matrix * r_matrix);
-LMS_daylight_stimuli = transpose(lms_matrix * s_daylight_matrix * r_matrix);
+LMS_incandescent_stimuli = LMS_stimuli(lms_matrix, s_incandescent_matrix , r_matrix,n_lamda);
+LMS_daylight_stimuli = LMS_stimuli(lms_matrix, s_daylight_matrix, r_matrix, n_lamda);
 
 
 LMS_incandescent_stimuli
@@ -94,3 +94,8 @@ opponency_incandescent(16,:)
 % % S_daylight_von_kries = plot(LMS_daylight_von_kries(:,[3]));
 % hold off
 % grid on
+
+
+function T_stimuli = LMS_stimuli(t,s,r,n_lamda)
+    T_stimuli = transpose(t*s*r*n_lamda)
+end
