07b3b2ed31a2a6982492360244ab991943e9e4d8
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-10-08 16:49:16 -0400

    Assignment 6 - All Questions Calculations Completed

--- Assignment 6 CIELAB/cielab.m
+++ Assignment 6 CIELAB/cielab.m
@@ -55,17 +55,18 @@ table_cc_D65 = createTablexyz(chromacity_coord_D65_two_deg);
 
 %% Question 3
 
+
 % Working
-% figure(1)
-% hold on
-% scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
-% scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
-% scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
-% axis equal
-% xlim([0 0.8]);
-% ylim([0 0.8]);
-% legend('A','D50','D65');
-% hold off
+figure(1)
+hold on
+scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
+scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
+scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
+axis equal
+xlim([0 0.8]);
+ylim([0 0.8]);
+legend('A','D50','D65');
+hold off
 
 
 % figure(2)
@@ -97,20 +98,21 @@ table_cc_D65 = createTablexyz(chromacity_coord_D65_two_deg);
 % hold off
 
 % Working
-% figure(3)
-% hold on
-% plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'b-x');
-% scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
-% scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
-% scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
-% axis equal
-% xlim([0 1]);
-% ylim([0 1]);
-% legend('A','D50','D65');
-% hold off
+figure(3)
+hold on
+plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'b-x');
+scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
+scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
+scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
+axis equal
+xlim([0 1]);
+ylim([0 1]);
+legend('A','D50','D65');
+hold off
 
 
 %% Question 6 (Bonus)
+
 %Get spectral radiance of Source A and Source D65
 sources_A = interpolateData(source_dataset{:,1},source_dataset.A,intp_wavelength_info);
 sources_D65 = interpolateData(source_dataset{:,1},source_dataset.D65,intp_wavelength_info);
@@ -120,8 +122,8 @@ wp_A_two_deg_source = calcTristimulusSource(xyz_std_obs_two_deg,sources_A,d_lamb
 wp_D65_two_deg_source = calcTristimulusSource(xyz_std_obs_two_deg,sources_D65,d_lambda);
 
 % Chromaticity Coordinates of Source A and Source D65 for 2 degree observer
-cc_wp_A_two_deg = calChromacityCoordinates(wp_A_two_deg_source')
-cc_wp_D65_two_deg = calChromacityCoordinates(wp_D65_two_deg_source')
+cc_wp_A_two_deg = calChromacityCoordinates(wp_A_two_deg_source');
+cc_wp_D65_two_deg = calChromacityCoordinates(wp_D65_two_deg_source');
 
 hold on
 plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'-');
@@ -135,9 +137,29 @@ hold off
 
 %% Question 7
 
-% Calculate X Prime, Y Prime and Z Prime
 
-X_Prime_A = 
+%Calculate L_star, a_star, b_star, C_star for all patches in A and D65 source 
+[L_A, a_A, b_A, C_A] = calcXYZtoCIELAB(tristimulus_XYZ_A_two_deg,wp_A_two_deg_source);
+[L_D65, a_D65, b_D65, C_D65] = calcXYZtoCIELAB(tristimulus_XYZ_D65_two_deg,wp_D65_two_deg_source);
+
+%Plot a against b under source A
+figure(5)
+hold on
+scatter(a_A',b_A');
+scatter(a_D65',b_D65');
+axis equal
+hold off
+
+figure(6)
+hold on
+scatter(C_A',L_A');
+scatter(C_D65',L_D65');
+axis equal
+hold off
+
+
+
+
 
 
 %% Functions
@@ -160,7 +182,7 @@ end
 function t = calcTristimulus(xyz_value,source,material,d_lambda)   
 
     %Calculate normalizing constant
-    k = (100/(source * xyz_value(:,2) * d_lambda))/100;
+    k = 100/(source * xyz_value(:,2) * d_lambda);
 
     s_lambda = diag(source);
 
@@ -190,6 +212,31 @@ function ts = calcTristimulusSource(xyz_value,source,d_lambda)
 %   t = custom_normalization(t);
 end
 
+
+%Function to calculate CIELAB from XYZ
+function [L_star,a_star,b_star,C_star] = calcXYZtoCIELAB(XYZ,whitepoint)
+    XYZ_Prime = calcXYZPrime(XYZ,whitepoint);
+    L_star = (116 * calConstants(XYZ_Prime(2,:))) - 16;
+    a_star= 500*(calConstants(XYZ_Prime(1,:)) - calConstants(XYZ_Prime(2,:)));
+    b_star = 200*(calConstants(XYZ_Prime(2,:)) - calConstants(XYZ_Prime(3,:)));
+    C_star = ((a_star).^2 + (b_star).^2).^(1/2);
+end
+
+
+% Calculate X Prime, Y Prime and Z Prime
+function XYZ_Prime = calcXYZPrime(XYZ,whitepoint)
+    XYZ_Prime = XYZ./whitepoint';
+end
+
+
+%Function to calculate constants in a and b
+function k = calConstants(x)
+    if (x > (24/116)^3)
+        k = (x).^(1/3);
+    else
+        k = ((841/108).*x) + (16/116);
+    end
+end
 
 
 %Function to create table and assign x,y,z
