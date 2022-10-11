deaf0f0cc85f37cea84c1ea6e6c88273d1a5b095
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-10-11 01:12:49 -0400

    Assignment 6 - Report done in Live Script

--- Assignment 6 CIELAB/cielab.m
+++ Assignment 6 CIELAB/cielab.m
@@ -29,10 +29,10 @@ tristimulus_XYZ_D50_two_deg = calcTristimulus(xyz_std_obs_two_deg,sources_D50,pa
 tristimulus_XYZ_D50_ten_deg = calcTristimulus(xyz_std_obs_ten_deg,sources_D50,patches,d_lambda);
 
 % chromacity_coordinates for all patches in D50 Source for 2 and 10 degree observer
-chromacity_coord_D50_two_deg = calChromacityCoordinates(tristimulus_XYZ_D50_two_deg);
-chromacity_coord_D50_ten_deg = calChromacityCoordinates(tristimulus_XYZ_D50_ten_deg);
+cc_D50_two_deg = calChromacityCoordinates(tristimulus_XYZ_D50_two_deg);
+cc_D50_ten_deg = calChromacityCoordinates(tristimulus_XYZ_D50_ten_deg);
 
-table_cc_D50 = createTablexyz(chromacity_coord_D50_two_deg);
+table_cc_D50 = createTablexyz(cc_D50_two_deg);
 
 %% Question 2
 
@@ -46,69 +46,34 @@ tristimulus_XYZ_A_two_deg = calcTristimulus(xyz_std_obs_two_deg,sources_A,patche
 tristimulus_XYZ_D65_two_deg = calcTristimulus(xyz_std_obs_two_deg,sources_D65,patches,d_lambda);
 
 % chromacity_coordinates for all patches in Source A and Source D65 for 2 degree observer
-chromacity_coord_A_two_deg = calChromacityCoordinates(tristimulus_XYZ_A_two_deg);
-chromacity_coord_D65_two_deg = calChromacityCoordinates(tristimulus_XYZ_D65_two_deg);
+cc_A_two_deg = calChromacityCoordinates(tristimulus_XYZ_A_two_deg);
+cc_D65_two_deg = calChromacityCoordinates(tristimulus_XYZ_D65_two_deg);
 
-table_cc_A = createTablexyz(chromacity_coord_A_two_deg);
-table_cc_D65 = createTablexyz(chromacity_coord_D65_two_deg);
+table_cc_A = createTablexyz(cc_A_two_deg);
+table_cc_D65 = createTablexyz(cc_D65_two_deg);
 
 
 %% Question 3
 
 
 % Working
-figure(1)
+h = figure(1);
 hold on
-scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
-scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
-scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
+scatter(table_cc_D50{:,1},table_cc_D50{:,2},32,'MarkerFaceColor','#0F4786');
+scatter(table_cc_A{:,1},table_cc_A{:,2},32,'MarkerEdgeColor','#930796','MarkerFaceColor','#930796');
+scatter(table_cc_D65{:,1},table_cc_D65{:,2},32,'MarkerFaceColor','#089E32');
+plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'-','LineWidth',2,'Color','black');
 axis equal
-xlim([0 0.8]);
-ylim([0 0.8]);
-legend('A','D50','D65');
+xlim([0 0.9]);
+ylim([0 0.9]);
+xlabel('x');
+ylabel('y');
+title('Chromaticity Coordinates of 24 Patches for D50, A, D65');
+legend('D50','A','D65','Spectrum Locus');
+dcm_obj = datacursormode(h);
+set(dcm_obj,'UpdateFcn',{@custom_label})
 hold off
 
-
-% figure(2)
-% plotChromaticity
-% hold on
-% scatter3(table_cc_A{:,1},table_cc_A{:,2},table_cc_A{:,3},24,'filled','Color','#8EB5E0');
-% hold on
-% scatter3(table_cc_D50{:,1},table_cc_D50{:,2},table_cc_D50{:,3},24,'filled','Color','#8EB5E0');
-% hold on
-% scatter3(table_cc_D65{:,1},table_cc_D65{:,2},table_cc_D65{:,3},24,'filled','Color','#8EB5E0');
-% xlabel('x');
-% ylabel('y');
-% zlabel('z');
-% grid on
-% axis equal
-% xlim([0 0.8]);
-% ylim([0 0.8]);
-% legend('A','D50','D65');
-% hold off
-
-% wp = calChromacityCoordinates(tristimulus_XYZ_D65_two_deg_source');
-% x_whitepoint = wp(1,:);
-% y_whitepoint = wp(2,:);
-% plotChromaticity
-% hold on
-% scatter(x_whitepoint,y_whitepoint,36,'filled','black');
-% scatter(table_cc_D65{:,1},table_cc_D65{:,2},24,'black');
-% axis equal
-% hold off
-
-% Working
-figure(3)
-hold on
-plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'b-x');
-scatter(table_cc_D50{:,1},table_cc_D50{:,2},20,'filled','Color','#8EB5E0');
-scatter(table_cc_A{:,1},table_cc_A{:,2},20,'filled','Color','#CEDAE8');
-scatter(table_cc_D65{:,1},table_cc_D65{:,2},20,'filled','Color','#4988CF');
-axis equal
-xlim([0 1]);
-ylim([0 1]);
-legend('A','D50','D65');
-hold off
 
 
 %% Question 6 (Bonus)
@@ -125,14 +90,22 @@ wp_D65_two_deg_source = calcTristimulusSource(xyz_std_obs_two_deg,sources_D65,d_
 cc_wp_A_two_deg = calChromacityCoordinates(wp_A_two_deg_source');
 cc_wp_D65_two_deg = calChromacityCoordinates(wp_D65_two_deg_source');
 
-hold on
-plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'-');
-scatter(cc_wp_A_two_deg(1,:),cc_wp_A_two_deg(2,:),36,'filled','black');
-scatter(table_cc_A{:,1},table_cc_A{:,2},24,'black');
-scatter(cc_wp_D65_two_deg(1,:),cc_wp_D65_two_deg(2,:),36,'filled','green');
-scatter(table_cc_D65{:,1},table_cc_D65{:,2},24,'green');
-axis equal
-hold off
+
+% Calculate the purity of each patch
+for i = 1:24
+    a = norm(cc_wp_A_two_deg(1:2,:)' - cc_D50_two_deg(1:2,i)');
+%     b = norm(cc_wp_A_two_deg(1:2,:)' - )
+end
+% 
+% figure(2)
+% hold on
+% plotChromaticity
+% plot(cc_spectral_locus_dataset{:,2},cc_spectral_locus_dataset{:,3},'-');
+% scatter(cc_wp_A_two_deg(1,:),cc_wp_A_two_deg(2,:),36,'filled','black');
+% scatter(cc_wp_D65_two_deg(1,:),cc_wp_D65_two_deg(2,:),36,'filled','green');
+% legend('A','D65');
+% axis equal
+% hold off
 
 
 %% Question 7
@@ -143,23 +116,58 @@ hold off
 [L_D65, a_D65, b_D65, C_D65] = calcXYZtoCIELAB(tristimulus_XYZ_D65_two_deg,wp_D65_two_deg_source);
 
 %Plot a against b under source A
-figure(5)
+h = figure(3);
 hold on
 scatter(a_A',b_A');
-scatter(a_D65',b_D65');
+xlabel('a^{*}');
+ylabel('b^{*}');
+title('CIELAB a^{*} vs b^{*} for 24 patches in A illuminant');
+xlim([-100 100]);
+ylim([-100 100]);
 axis equal
+grid on
+dcm_obj = datacursormode(h);
+set(dcm_obj,'UpdateFcn',{@custom_label})
 hold off
 
-figure(6)
+h = figure(4);
 hold on
 scatter(C_A',L_A');
-scatter(C_D65',L_D65');
+xlabel('Chroma (C^{*}_{ab})');
+ylabel('Lightness (L^{*}_{ab})');
+title('CIELAB L^{*}_{ab} vs C^{*}_{ab} for 24 patches in A illuminant');
 axis equal
+grid on
+dcm_obj = datacursormode(h);
+set(dcm_obj,'UpdateFcn',{@custom_label})
 hold off
 
 
+h = figure(5);
+hold on
+scatter(a_D65',b_D65');
+xlabel('a^{*}');
+ylabel('b^{*}');
+title('CIELAB a^{*} vs b^{*} for 24 patches in D65 illuminant');
+xlim([-100 100]);
+ylim([-100 100]);
+axis equal
+grid on
+dcm_obj = datacursormode(h);
+set(dcm_obj,'UpdateFcn',{@custom_label})
+hold off
 
-
+h = figure(6);
+hold on
+scatter(C_D65',L_D65');
+xlabel('Chroma (C^{*}_{ab})');
+ylabel('Lightness (L^{*}_{ab})');
+title('CIELAB L^{*}_{ab} vs C^{*}_{ab} for 24 patches in D65 illuminant');
+axis equal
+grid on
+dcm_obj = datacursormode(h);
+set(dcm_obj,'UpdateFcn',{@custom_label})
+hold off
 
 
 %% Functions
@@ -182,7 +190,7 @@ end
 function t = calcTristimulus(xyz_value,source,material,d_lambda)   
 
     %Calculate normalizing constant
-    k = 100/(source * xyz_value(:,2) * d_lambda);
+    k = (100/(source * xyz_value(:,2) * d_lambda))/100;
 
     s_lambda = diag(source);
 
@@ -204,7 +212,7 @@ end
 function ts = calcTristimulusSource(xyz_value,source,d_lambda)   
 
     %Calculate normalizing constant
-    k = 100/(source * xyz_value(:,2) * d_lambda);
+    k = (100/(source * xyz_value(:,2) * d_lambda))/100;
 
     %Calculating tristimulus
     ts = k.*((source*xyz_value))*d_lambda;
@@ -215,7 +223,7 @@ end
 
 %Function to calculate CIELAB from XYZ
 function [L_star,a_star,b_star,C_star] = calcXYZtoCIELAB(XYZ,whitepoint)
-    XYZ_Prime = calcXYZPrime(XYZ,whitepoint);
+    XYZ_Prime = calcXYZPrime(XYZ,whitepoint)
     L_star = (116 * calConstants(XYZ_Prime(2,:))) - 16;
     a_star= 500*(calConstants(XYZ_Prime(1,:)) - calConstants(XYZ_Prime(2,:)));
     b_star = 200*(calConstants(XYZ_Prime(2,:)) - calConstants(XYZ_Prime(3,:)));
@@ -243,6 +251,16 @@ end
 function a2t = createTablexyz(values)
     a2t = array2table(values');
     a2t.Properties.VariableNames(1:3) = {'x','y','z'};
+end
+
+
+% Function to customize text of data tips in the plot
+function txt = custom_label(~,event_obj)
+pos = get(event_obj,'Position');
+I = get(event_obj, 'DataIndex');
+txt = {['X: ',num2str(pos(1))],...
+       ['Y: ',num2str(pos(2))],...
+       ['Patch: ',num2str(I)]};
 end
 
 % Function to normalize a matrix with its peak value (not using default normalize function)
