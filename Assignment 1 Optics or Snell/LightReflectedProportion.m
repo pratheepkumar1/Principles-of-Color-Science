5960294f1f41bfdd91a860cf112f099bae577cae
Pratheep Kumar <pratheepkumar1@gmail.com>
2022-11-14 19:43:04 -0500

    Resubmission A1: Updated the equation error and report

--- Assignment 1 Optics or Snell/LightReflectedProportion.m
+++ Assignment 1 Optics or Snell/LightReflectedProportion.m
@@ -1,23 +1,33 @@
-n_material = 1.6410;
+% prompt = "Enter n_material = ";
+% n_material = input(prompt);
+% prompt = "Enter n_air = ";
+% n_air = input(prompt);
+% prompt = "Enter angle of incidence (in deg) = ";
+% angle_incidence = input(prompt);
+
+n_material = [1.6410 1.6226 1.6175];
 n_air = 1;
 angle_incidence = 55;
 
+for i = 1 : length(n_material)
+
 r_parallel = ((cosd(angle_incidence) - ...
-    sqrt((n_material/n_air)^2 - sind(angle_incidence ^ 2))) ...
+    sqrt((n_material(i)/n_air)^2 - sind(angle_incidence)^2)) ...
     / ...
     (cosd(angle_incidence) + ...
-    sqrt((n_material/n_air)^2 - sind(angle_incidence ^ 2))))^2 ;
+    sqrt((n_material(i)/n_air)^2 - sind(angle_incidence)^2)))^2 ;
 
 
-r_perpendicular = ((((n_material/n_air)^2)*(cosd(angle_incidence)) - ...
-    sqrt((n_material/n_air)^2 - sind(angle_incidence ^ 2))) ...
+r_perpendicular = ((((n_material(i)/n_air)^2)*(cosd(angle_incidence)) - ...
+    sqrt((n_material(i)/n_air)^2 - sind(angle_incidence)^2)) ...
     / ...
-    (((n_material/n_air)^2)*(cosd(angle_incidence)) + ...
-    sqrt((n_material/n_air)^2 - sind(angle_incidence ^ 2))))^2 ;
+    (((n_material(i)/n_air)^2)*(cosd(angle_incidence)) + ...
+    sqrt((n_material(i)/n_air)^2 - sind(angle_incidence)^2)))^2 ;
 
 surface_reflection = ((r_parallel + r_perpendicular ) / 2) * 100;
-
 disp(surface_reflection)
+
+end
 
 
 % Calculations
